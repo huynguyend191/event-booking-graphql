0b3c538ec22e85f7c336b5bdf77289cc8dbc6df2
sneezingcat191 <huynguyend191@gmail.com>
2019-01-09 17:36:04 +0700

    add user authentication

--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -6,7 +6,10 @@ const { transformBooking, transformEvent } = require('./merge');
 
 module.exports = {
 
-  bookings: async () => {
+  bookings: async (req) => {
+    if (!req.isAuth) {
+      throw new Error('Unauthenticated');
+    }
     try {
       const bookings = await Booking.find();
       return bookings.map(booking => {
@@ -17,17 +20,23 @@ module.exports = {
     }
   },
 
-  bookEvent: async args => {
+  bookEvent: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error('Unauthenticated');
+    }
     const fetchedEvent = await Event.findOne({ _id: args.eventId });
     const booking = new Booking({
-      user: '5c34c538e86b740cc0306a4e',
+      user: req.userId,
       event: fetchedEvent
     });
     const result = await booking.save();
     return transformBooking(result);
   },
 
-  cancelBooking: async args => {
+  cancelBooking: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error('Unauthenticated');
+    }
     try {
       const booking = await Booking.findById(args.bookingId).populate('event');
       const event = transformEvent(booking.event);
--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,4 +1,6 @@
 const Event = require('../../models/event');
+const User = require('../../models/user');
+
 const { user } = require('./merge');
 
 const { transformEvent } = require('./merge');
@@ -15,19 +17,22 @@ module.exports = {
     }
   },
 
-  createEvent: async (args) => {
+  createEvent: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error('Unauthenticated');
+    }
     const event = new Event({
       title: args.eventInput.title,
       description: args.eventInput.description,
       price: +args.eventInput.price,
       date: new Date(args.eventInput.date),
-      creator: '5c34c538e86b740cc0306a4e'
+      creator: req.userId
     });
     let createdEvent;
     try {
       const result = await event.save();
       createdEvent = transformEvent(result);
-      const creator = await User.findById('5c34c538e86b740cc0306a4e');
+      const creator = await User.findById(req.userId);
       if (!creator) {
         throw new Error('User not found');
       }
